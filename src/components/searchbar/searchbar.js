935a1ed56565094119dbbd79efbae0a9d0e91c0f
BUMI0 <79413195+BUMI0@users.noreply.github.com>
2022-05-19 20:02:25 +0200

    Update searchbar.js

--- src/components/searchbar/searchbar.js
+++ src/components/searchbar/searchbar.js
@@ -2,11 +2,29 @@ import React from 'react';
 
 import './searchbar.css';
 
-class searchbar extends React.Component{
+class SearchBar extends React.Component{
+    constructor(props){
+        super(props);
+        
+        this.state = {
+            term: ''
+        }
+        
+        this.search = this.search.bind(this);
+        this.handleTermChange = this.handleTermChange.bind(this);
+    }
+    search() {
+        this.props.onSearch(this.state.term);
+    }
+    
+    
+    handleTermChange(event){
+        this.setState({term: event.target.value});
+    }
     render(){
         return (
             <div className="SearchBar">
-                <input placeholder="Suchbegriff eingeben" />
+                <input onChange = {this.handleTermChange} placeholder="Suchbegriff eingeben" />
                 <button className="SearchButton">SUCHE STARTEN</button>
             </div>
         )
