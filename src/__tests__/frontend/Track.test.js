a25d34a6d2d34132cfe46c4a03dbda65ae2d4e6c
Tim Kanbur <104684239+timkanbur@users.noreply.github.com>
2022-05-27 21:24:54 +0200

    Added a Test of the Testsuite, works
    Signed-off-by: Tim Kanbur <104684239+timkanbur@users.noreply.github.com>

--- src/__tests__/frontend/Track.test.js
+++ src/__tests__/frontend/Track.test.js
@@ -9,7 +9,6 @@ test('loads and displays greeting', async () => {
 								onAdd={()=> {console.log("Adding")}}
 								onRemove={()=> {console.log("Removing")}}
 								track={{name:"TestName", artist:"TestArtist", album:"TestAlbum"}} />);
-	console.log(test.container.innerHTML)
 	// <div class="Track"><div class="Track-action"><h3>TestName</h3><p>TestArtist | TestAlbum</p></div><button class="Track-action">+</button></div>
 	expect(test.container.innerHTML == '<div class="Track"><div class="Track-action"><h3>TestName</h3><p>TestArtist | TestAlbum</p></div><button class="Track-action">+</button></div>')
 })
