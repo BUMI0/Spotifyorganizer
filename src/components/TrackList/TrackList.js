f19ffd75e2ddbc58fae6779bf4232e1ba2e3b9c6
BUMI0 <79413195+BUMI0@users.noreply.github.com>
2022-05-28 00:48:59 +0200

    Update TrackList.js

--- src/components/TrackList/TrackList.js
+++ src/components/TrackList/TrackList.js
@@ -8,7 +8,8 @@ class TrackList extends React.Component{
         return (
             <div className="TrackList">
                 {
-                    this.props.tracks.map(track => {
+                 //für alle objekte im this.props.tracks array wird Track Komponent returned  
+                this.props.tracks.map(track => {
                         return <Track track={track}
                             key={track.id} 
                             onAdd={this.props.onAdd}
