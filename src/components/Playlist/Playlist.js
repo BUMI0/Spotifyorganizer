7c0ff8350c99e22b0c303e0e650c72bf7c6e1817
BUMI0 <79413195+BUMI0@users.noreply.github.com>
2022-06-08 00:40:34 +0200

    Update Playlist.js

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -7,13 +7,14 @@ class Playlist extends React.Component {
     constructor(props){
         super(props);
         this.handleNameChange = this.handleNameChange.bind(this);
-    }
+    //event wird durch eingabe des Nutzers ausgelöst(getriggert) wird -> durch onChange Attribut im input Element
     handleNameChange(event){
         this.props.onNameChange(event.target.value);
     }
     render (){
         return (
             <div className="Playlist">
+            //Methode an das onChange property passen
             <input defaultValue={"New Playlist"} onChange = {this.handleNameChange}/>
             <TrackList tracks={this.props.playlistTracks}
                         onRemove={this.props.onRemove}
